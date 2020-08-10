dc8127641bfac704a53b02efa676c594e2e478a8
Aishwarya Manicka <manickaa@oregonstate.edu>
2020-08-10 08:15:39 -0700

    Renders the components for app

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+import Board from './Components/Board/Board';
+
+class App extends Component {
+  render() {
+    return (
+      <Board />
+    );
+  }
+}
+
+export default App;
